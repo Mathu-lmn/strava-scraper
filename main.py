c037ef9c60facd097ffdd1837723e28d9e4da90b
Mathu-lmn <80094438+Mathu-lmn@users.noreply.github.com>
2024-07-28 21:02:20 +0200

    Update weighted map script

--- main.py
+++ main.py
@@ -9,22 +9,11 @@ from login import login
 # Strava REST API wrapper
 from stravalib.client import Client as StravaClient
 
-color_intensity = [
-    "#FF0000", # red
-    "#FF4500", # orange red
-    "#FFA500", # orange
-    "#FFD700", # gold
-    "#FFFF00", # yellow
-    "#ADFF2F", # green yellow
-    "#008000", # green
-    "#006400", # dark green
-    "#0000FF", # blue
-    "#00008B", # dark blue
-    "#8A2BE2", # blue violet
-]
+color_intensity = ['#0000ff', '#1c00e5', '#3800cc', '#5500b2', '#710099', '#8d007f', '#a90066', '#c5004c', '#e20033', '#ff0019']
 
 
-def generate_map_weighted(activities_map):
+# Experimental : Higher radius but only one circle gets overlapped per polyline
+def generate_map_weighted(activities_map, experimental=False):
     list_of_poly = []
     for file in os.listdir("cache"):
         if file.startswith("map-"):
@@ -34,7 +23,7 @@ def generate_map_weighted(activities_map):
 
     
     coords = [(tuple(coord), i) for i, poly in enumerate(list_of_poly) for coord in poly]
-    
+
     # Extract just the coordinates for KDTree
     points = np.array([coord for coord, _ in coords])
     
@@ -44,28 +33,34 @@ def generate_map_weighted(activities_map):
     
     for coord, i in coords:
         # Query the KDTree for nearby points within the specified distance
-        indices = tree.query_ball_point(coord, np.sqrt(0.2))
-        
+        distance = experimental and 0.0005 or 0.00015
+        indices = tree.query_ball_point(coord, distance, return_sorted=True)
+
         value = 1
         keys_to_delete = []
-        
+        treated_polys = []
+
         # Check the nearby points
         for idx in indices:
             nearby_coord, poly_index = coords[idx]
-            if poly_index != i:
+            if poly_index != i and poly_index not in treated_polys:
                 value += coord_dict.get(nearby_coord, (0, poly_index))[0]
+                treated_polys.append(poly_index)
+                keys_to_delete.append(nearby_coord)
+            elif not experimental:
                 keys_to_delete.append(nearby_coord)
         
         # Delete the keys that are too close
         for key in keys_to_delete:
             if key in coord_dict:
                 del coord_dict[key]
-        
+
         coord_dict[coord] = (value, i)
 
+    highest = max(coord_dict.values(), key=lambda x: x[0])[0]
     for coord, (value, _) in coord_dict.items():
         # The higher the value, the darker the color
-        color = color_intensity[min(value, len(color_intensity) - 1)]
+        color = color_intensity[min(int(value / highest * len(color_intensity)), len(color_intensity) - 1)]
         folium.CircleMarker(coord, radius=5, color=color, fill_color=color).add_to(activities_map)
 
 
@@ -77,19 +72,27 @@ def generate_full_map(activities_map):
                 line = polyline.decode(f.read())
                 folium.PolyLine(line).add_to(activities_map)
 
-def global_map():
+def global_map(weighted=False):
     create_map_cache()
 
     activities_map = folium.Map(location=[48.8566, 2.3522], zoom_start=5)
 
-    # generate_map_weighted(activities_map) # This will REMOVE the polylines and generate circles with weights - HEAVY !!
-    generate_full_map(activities_map) # This will ADD the polylines but no weights (overlapping lines)
+    if weighted:
+        generate_map_weighted(activities_map, False) # This will REMOVE the polylines and generate circles with weights - HEAVY !!
+    else:
+        generate_full_map(activities_map) # This will ADD the polylines but no weights (overlapping lines)
 
     activities_map.save("out/activities_map.html")
+
+    if weighted:
+        generate_map_weighted(activities_map, True)
+        activities_map.save("out/activities_map_experimental.html")
 
     # open the map in the browser
     import webbrowser
     webbrowser.open(os.path.abspath("out/activities_map.html"))
+    if weighted:
+        webbrowser.open(os.path.abspath("out/activities_map_experimental.html"))
 
 
 
@@ -117,6 +120,6 @@ if __name__ == "__main__":
         print("Error: {0}".format(e))
         exit(1)
 
-    global_map()
+    global_map(True)
 
     
